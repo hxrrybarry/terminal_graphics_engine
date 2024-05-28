8b5c1125b2f437b7fce1503549aa1c826c0c3d4e
Harrison O'Leary <145754861+hxrrybarry@users.noreply.github.com>
2024-05-28 17:12:50 +0100

    added some sprites

--- Program.cs
+++ Program.cs
@@ -9,32 +9,64 @@ internal class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
 
-        Scene sc = new(125, 125);
+        Scene sc = new(100, 54);
 
-        // Sprite obj = new("#\n#\n#m", 10, 20);
-        // Sprite obj2 = new("#####", 10, 25);
-
-        string playerTexture = File.ReadAllText(@"C:\Users\harib\source\repos\terminal_graphics_engine\piece1.txt");
+        string playerTexture = File.ReadAllText(@"C:\Users\HarrisonO’Leary\Source\Repos\terminal_graphics\piece1.txt");
         playerTexture = playerTexture.Replace("\r", "");
 
-        Sprite obj = new(playerTexture, 20, 20);
+        string clockFace = File.ReadAllText(@"C:\Users\HarrisonO’Leary\Source\Repos\terminal_graphics\clcok-face.txt");
+        clockFace = clockFace.Replace("\r", "");
+
+        const string secondHand = "####";
+        const string hour = "@@";
+
+        Sprite clock = new(clockFace, 40, 40, false);
+        Sprite handSecond = new(secondHand, 61, 46);
+        Sprite hourHand = new(hour, 61, 46);
+
+        Sprite obj = new(playerTexture, 30, 30);
+
+        string cross = File.ReadAllText(@"C:\Users\HarrisonO’Leary\Source\Repos\terminal_graphics\line.txt");
+        cross = cross.Replace("\r", "");
+
+        Sprite crossHair = new(cross, 65, 30);
 
         List<Sprite> spritesToRender = new();
+        spritesToRender.Add(clock);
+        spritesToRender.Add(handSecond);
+        spritesToRender.Add(hourHand);
         spritesToRender.Add(obj);
-        // spritesToRender.Add(obj2);
-        // spritesToRender.Add(obj3);
+        spritesToRender.Add(crossHair);
+
         string renderString = sc.GetRenderString(spritesToRender);
         Console.Write(renderString);
+
+        Vector2 pivot = new(0, 0);
 
         // main loop
         while (true)
         {
+            spritesToRender[1].Rotate(0.05f, pivot);
+            spritesToRender[2].Rotate(0.0025f, pivot);
+            spritesToRender[3].Rotate(0.025f, spritesToRender[3].Center);
+
+            spritesToRender[4].Rotate(0.05f, spritesToRender[4].Center);
+
+            try
+            {
+                renderString = sc.GetRenderString(spritesToRender);
+            }
+            catch { }
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write(renderString);
+
             if (Console.KeyAvailable)
             {
                 ConsoleKey key = Console.ReadKey(true).Key;
 
-                Vector2 pivot = spritesToRender[0].Center;
-                // Vector2 pivot = new(0, 0);
+                // Vector2 pivot = spritesToRender[0].Center;
+                
 
                 switch (key)
                 {
@@ -56,14 +88,10 @@ internal class Program
                         spritesToRender[0].Rotate(0.05f, pivot); break;
                 }
 
-                try
-                {
-                    renderString = sc.GetRenderString(spritesToRender);
-                } catch { }
                 
-                Console.SetCursorPosition(0, 0);
-                Console.Write(renderString);
             }
+
+            Thread.Sleep(20);
         }
     }
 }
